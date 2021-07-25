a0022e33937bf392e8346f621adfbb2c0bca47ac
Aadhi Nana <ajibaa@gmail.com>
2021-07-25 14:46:02 +0530

    can draw now

---
+++ draw.js
@@ -0,0 +1,37 @@
+const canvas = document.querySelector("canvas");
+
+let ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let isDrawing = false;
+
+canvas.onmousedown = startDrawing;
+
+function startDrawing(e) {
+  console.log("hello");
+  isDrawing = true;
+
+  ctx.beginPath();
+  ctx.lineWidth = 2;
+  ctx.moveTo(e.clientX, e.clientY);
+}
+
+canvas.onmousemove = draw;
+
+function draw(e) {
+  if (!isDrawing) return;
+  ctx.lineTo(e.clientX, e.clientY);
+  ctx.lineCap = "round";
+  ctx.stroke();
+}
+
+canvas.onmouseup = finishDrawing;
+
+function finishDrawing(e) {
+  if (!isDrawing) return;
+  ctx.lineTo(e.clientX, e.clientY);
+  ctx.stroke();
+  ctx.closePath();
+  isDrawing = false;
+}
