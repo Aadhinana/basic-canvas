e37f7060af6cea80814410be7b6ff52e2d47b579
Aadhi Nana <ajibaa@gmail.com>
2021-07-26 17:07:50 +0530

    fix: resize canvas handle

--- draw.js
+++ draw.js
@@ -4,14 +4,27 @@ let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+console.log(canvas);
+
 let isDrawing = false;
+
+window.addEventListener("resize", () => {
+  console.log("sadf")
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  console.log(canvas.width, canvas.height);
+});
+
+document.addEventListener("mousemove", (e) => {
+  // console.log("MOUSEMOVUE", e.clientX, e.clientY);
+});
 
 canvas.onmousedown = startDrawing;
 
 function startDrawing(e) {
   console.log("hello");
   isDrawing = true;
-
+  console.log("START", e.clientX, e.clientY);
   ctx.beginPath();
   ctx.lineWidth = 2;
   ctx.moveTo(e.clientX, e.clientY);
@@ -21,6 +34,7 @@ canvas.onmousemove = draw;
 
 function draw(e) {
   if (!isDrawing) return;
+  console.log("SRAWING", e.clientX, e.clientY);
   ctx.lineTo(e.clientX, e.clientY);
   ctx.lineCap = "round";
   ctx.stroke();
