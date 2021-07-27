2bfc669bc000d91f8e72848876ddb1e7e33ad573
Aadhi Nana <ajibaa@gmail.com>
2021-07-27 14:17:54 +0530

    basic collision detec

--- collision-detection/app.js
+++ collision-detection/app.js
@@ -21,14 +21,64 @@ window.addEventListener("mousemove", (e) => {
   mouse.y = e.clientY;
 });
 
-function init() {}
+class Cirlce {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+  update() {
+    this.draw();
+  }
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fillStyle = this.color;
+    context.fill();
+    context.stroke();
+    context.closePath();
+  }
+}
+
+let c1;
+let c2;
+function init() {
+  c1 = new Cirlce(window.innerWidth / 2, window.innerHeight / 2, 200, "red");
+  c2 = new Cirlce(10, 10, 20, "black");
+}
 
 function animate() {
   context.clearRect(0, 0, canvas.width, canvas.height);
 
-  context.fillText("In the canvas", mouse.x, mouse.y);
+  c1.update();
+  c2.x = mouse.x;
+  c2.y = mouse.y;
+  c2.update();
+
+  //   console.log(getDistance(c1.x, c1.y, c2.x, c2.y));
+  // if touching -> distance is less than radius of both
+  if (getDistance(c1.x, c1.y, c2.x, c2.y) < c1.radius + c2.radius) {
+    c2.color = "black";
+  } else {
+    c2.color = "blue";
+  }
+
 
   requestAnimationFrame(animate);
 }
 
+init();
 animate();
+
+// Helpers
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getDistance(x1, y1, x2, y2) {
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
